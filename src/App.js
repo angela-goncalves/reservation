4a231dbf0119e79cf1af2140a880982ae7385247
angela-goncalves <angelakgo20@gmail.com>
2021-08-02 18:16:10 -0300

    change image and add State and Reducer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
 import "./App.css";
+import React, { useState, useReducer } from "react";
 import Reservation from "./components/Reservation";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import image from "./plateHeader.png";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import useLocalStorage from "../useLocalStorage";
 
+const ACTIONS = {
+  ADD_TODO: "add-todo",
+  DELETE_TODO: "delete-todo",
+  TAGGLE_TODO: "taggle-todo",
+};
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [];
+    case ACTIONS.DELETE_TODO:
+      return {};
+    case ACTIONS.TAGGLE_TODO:
+      return {};
+    default:
+      return todos;
+  }
+};
+
 function App() {
-  // const [tasks, setTasks] = useLocalStorage("tasks", inicialValue);
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    people: "",
+    date: "",
+    time: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO });
+    setValue("");
+  };
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    console.log("state:", value);
+    console.log(name, value);
+
+    setValue((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
   return (
     <Router>
-      <div className="pl-10 bg-black">
+      <div className="w-ful px-6 pb-4 bg-black flex flex-col items-center lg:pl-44 lg:pr-0 lg:items-start lg:relative">
+        <img
+          src={image}
+          alt="Logo"
+          className="w-full max-w-md lg:max-w-lg lg:absolute lg:-right-28 lg:transform lg:rotate-90"
+        />
         <Nav />
         <Header />
         <Switch>
-          <Route path="/reservation" component={Reservation}></Route>
+          <Router path="/" exact component={App} />
+          <Route path="/reservation" component={Reservation} />
         </Switch>
+        <Reservation
+          todos={todos}
+          handleSubmit={handleSubmit}
+          value={value}
+          setValue={setValue}
+          handleOnChange={handleOnChange}
+          dispatch={dispatch}
+        />
       </div>
     </Router>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,11 @@
 import { Link } from "react-router-dom";
-import image from "../plateHeader.png";
 
 export default function Nav() {
   return (
-    <div className="z-0">
-      <div className="relative ">
-        <Link to="/reservation">
-          <p className="py-10 text-4xl text-white font-bold">dine</p>
-        </Link>
-        <img
-          src={image}
-          alt="Logo"
-          className="w-full max-w-2xl absolute top-0 right-0 "
-        />
-      </div>
+    <div className="flex flex-col items-center">
+      <p className="py-10 text-4xl text-white font-bold">
+        <Link to="/">dine</Link>
+      </p>
     </div>
   );
 }
